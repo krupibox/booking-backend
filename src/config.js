3418537b84fd9841f3ab281cb68a4944333ed712
Ilya Krupin <dev@krupibox.ru>
2020-12-14 19:59:06 +0300

    set db password

--- src/config.js
+++ src/config.js
@@ -16,7 +16,7 @@ const config = {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || '3306',
     username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
+    password: process.env.DB_PASSWORD || 'root',
     database: process.env.DB_DATABASE || 'booking',
     dialect: 'mysql',
   },
