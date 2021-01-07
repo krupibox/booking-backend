1d1ee4b2381a76609aa3def3f1d210f3f2007c65
Ilya Krupin <dev@krupibox.ru>
2021-01-07 20:09:43 +0300

    Delete password

--- src/config.js
+++ src/config.js
@@ -16,7 +16,7 @@ const config = {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || '3306',
     username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_DATABASE || 'booking',
     dialect: 'mysql',
   },
